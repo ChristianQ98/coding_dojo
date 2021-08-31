eda50e16615502e387d957e0f17062d1c10221ec
Christian Quintero <78188432+ChristianQ98@users.noreply.github.com>
2021-08-31 10:12:40 -0700

    Add files via upload

---
+++ mern/react/intro/firstjsx/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <ul><h2>Things I need to do:</h2></ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+    </div>
+  );
+}
+
+export default App;
